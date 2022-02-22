03647285b45f962f9a255910685ca993bbae8c7d
Vitaly Kostikov <one.kostikov@gmail.com>
2022-02-22 16:04:20 +0100

    Initial Commit

---
+++ SelfApp/SelfAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfAppApp.swift
+//  SelfApp
+//
+//  Created by Vitaly on 22/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SelfAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
